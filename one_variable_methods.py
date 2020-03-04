83b6642fa64042bedce0a4795442d2b56688b3c6
Snopoff <snopoffp@gmail.com>
2020-03-04 13:57:50 +0300

    added Simpson and Newton methods, fixed gss

--- one_variable_methods.py
+++ one_variable_methods.py
@@ -1,59 +1,144 @@
 from typing import Callable
 from math import sqrt
-
-from numpy import linspace
-from matplotlib.pyplot import plot, show
+import numpy as np
 
 
-def bisection(f: Callable, left=0.0, right=1.0, eps=0.001) -> float:
-    '''function that performs bisection method with given function'''
-    if f(left) * f(right) > 0:
+def bisection(f: Callable, a=0.0, b=1.0, eps=0.001) -> float:
+    '''
+    Bisection method
+
+        Parameters
+        ----------
+        f : given function
+        a : left border of an interval
+        b : right border of an interval
+        eps : accuracy
+
+        Returns
+        -------
+        float : approximation for the root of a function
+
+    '''
+    if f(a) * f(b) > 0:
         return None
     else:
-        mid = (right+left) / 2.0
-        while (right-left) / 2.0 > eps:
-            print(mid, eps)
+        mid = (b+a) / 2.0
+        while (b-a) / 2.0 > eps:
             if abs(f(mid)) < eps:
                 return mid
-            elif f(left) * f(mid) < 0.0:
+            elif f(a) * f(mid) < 0.0:
                 '''there is a root between two points'''
-                right = mid
+                b = mid
             else:
-                left = mid
-            mid = (right+left) / 2.0
+                a = mid
+            mid = (b+a) / 2.0
         return mid
 
 
-def gss(f: Callable, left=0.0, right=1.0, eps=0.001) -> float:
-    '''function that performs golden section search with given function'''
+def gss(f: Callable, a=0.0, b=1.0, eps=0.001) -> float:
+    '''
+    Golden section search
+
+        Parameters
+        ----------
+        f : given function
+        a : left border of an interval
+        b : right border of an interval
+        eps : accuracy
+
+        Returns
+        -------
+        float : approximation for the minima of a function
+
+    '''
     gr = (1 + sqrt(5)) / 2
-    c = right - (right - left) / gr
-    d = left + (right - left) / gr
+    c = b - (b - a) / gr
+    d = a + (b - a) / gr
     while abs(c - d) > eps:
         if f(c) < f(d):
-            left = d
+            b = d
         else:
-            right = c
+            a = c
 
          # We recompute both c and d here to avoid loss of precision which may lead to incorrect results or infinite loop
-        c = right - (right - left) / gr
-        d = left + (right - left) / gr
+        c = b - (b - a) / gr
+        d = a + (b - a) / gr
 
-    return (right + left) / 2
+    return (b + a) / 2
 
 
-def plot_func(f: Callable, left=0.0, right=1.0):
-    x = linspace(left, right)
-    y = [f(a) for a in x]
-    plot(x, y)
-    show()
+def Simpson(f: Callable, a=0.0, b=1.0, n=100) -> float:
+    '''
+    Simpson's method
+
+        Parameters
+        ----------
+        f : given function
+        a : left border of an interval
+        b : right border of an interval
+        n : number of points 
+
+        Returns
+        -------
+        float : approximation for the integral
+
+    '''
+
+    result = 1.0
+    dx = (b - a)/n
+    x = np.linspace(a, b, num=n)
+    for i in range(n):
+        result += f(x[i]) if i == 0 or i == n-1 else 2*((i % 2+1))*f(x[i])
+    result *= dx/3
+    return result
+
+
+def Newton(f: Callable, df: Callable, x0=1.0, eps=1e-8, max_iter=1e6) -> float:
+    '''
+    Newton's method
+
+        Parameters
+        ----------
+        f : given function
+        df : derivative of a function
+        x0 : initial guess for a solution f(x)=0
+        eps : accuracy
+        max_inter : maximum number of iterations
+
+        Returns
+        -------
+        float : approximation for the root of the function
+
+    '''
+
+    result = x0
+    for n in range(int(max_iter)):
+        f_val = f(result)
+        if abs(f_val) < eps:
+            return result
+        df_val = df(result)
+        if df_val == 0:
+            return None
+        result = result - f_val/df_val
+
+    return None
 
 
 if __name__ == "__main__":
     def f(x):
         a = 5
-        b = -2
-        c = 3
+        b = 1
+        c = -3
         return a*x**2 + b*x + c
-    print(bisection(f, -2.0, 2.0))
-    print(gss(f, -2.0, 2.0))
+
+    def df(x):
+        a = 5
+        b = 1
+        c = -3
+        return 2*a*x + b
+    a = 0.0
+    b = 4.0
+    print(bisection(f, a, b))
+    print(gss(f, a, b))
+    print(Simpson(f, a, b))
+    print(Newton(f, df))
